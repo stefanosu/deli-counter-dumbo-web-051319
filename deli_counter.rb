78d916124ded99b878688896cac7d86da21c0862
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 20:51:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli == 0
-    puts "There is nobody waiting to be served!"!"
+    puts "There is nobody waiting to be served!!"
   else 
 	 puts "Currently serving " + katz_deli[0] +"."
   end
