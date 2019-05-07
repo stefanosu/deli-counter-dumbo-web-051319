ccbc17e698f0b554505fb287bddc2c1b153e056f
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 20:56:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(numinline)
     puts "The line is currently empty."
   else
     line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index} #{name}")
+      line_array.push("#{index} #{name}")
       # push method will push the name into the index 
     end
     puts "The line is: #{line_array.join(" ")}"
