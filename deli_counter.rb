8a4ed811112e9b17a6342d63e644c9e7de53547b
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 20:36:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,4 @@
-katz_deli = []
 
-def line(katz_deli)
-  if !katz_deli.empty? == true 
-  puts "The line is currently empty."
-  end 
-end
-
-
-def take_a_number(katz_deli, name) 
-  
-end 
-
-
-def now_serving 
-
-end
 
 
 
