cc986b8ca75fc6756bc50a595d16f56c8115ef80
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 19:43:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line 
   katz_deli = []
   if katz_deli.empty? == true 
-  puts "The line is currently empty"
+  return puts "The line is currently empty"
 end 
 
 def take_a_number(katz_deli, name) 
